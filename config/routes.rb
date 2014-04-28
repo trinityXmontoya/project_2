a3276f47342c1d8edbbd71086cbc9f3f76378e5d
sjstebbins <sjstebbins@aol.com>
2014-04-28 17:03:55 -0400

    User_profile_view

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 Project2::Application.routes.draw do
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
 
->>>>>>> 41846627a73503e846be951c1913e8d1da5e721c
 
   root to: 'auctions#index'
   resources :users
@@ -11,35 +7,6 @@ Project2::Application.routes.draw do
   resources :categories
   resources :auctions
   resources :messages
-<<<<<<< HEAD
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
-=======
-  get "messages/index"
-  get "messages/show"
-  get "messages/new"
-  get "messages/edit"
-  get "categories/index"
-  get "categories/show"
-  get "categories/new"
-  get "categories/edit"
-  get "bids/index"
-  get "bids/show"
-  get "bids/new"
-  get "bids/edit"
-  get "auctions/index"
-  get "auctions/show"
-  get "auctions/new"
-  get "auctions/edit"
-  get "users/index"
-  get "users/show"
-  get "users/new"
-  get "users/edit"
->>>>>>> 9b32de2c71e8d3bc2fec99cbf3e288449c298fa6
-=======
->>>>>>> 41846627a73503e846be951c1913e8d1da5e721c
-
-
 
 get '/auth/:facebook/callback', to: 'sessions#create'
 
