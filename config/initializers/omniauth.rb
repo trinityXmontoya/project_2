2d1bb82b26fd5935dd4bed967107efe4b8a70914
trinityXmontoya <trinityXmontoya@gmail.com>
2014-04-28 11:26:56 -0400

    add basic_info scope to omniauth specs

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, ENV['FACEBOOK_PROJECT_2_ID'], ENV['FACEBOOK_PROJECT_2_SECRET']
+  provider :facebook, ENV['FACEBOOK_PROJECT_2_ID'], ENV['FACEBOOK_PROJECT_2_SECRET'],
+    scope: 'basic_info'
 end
