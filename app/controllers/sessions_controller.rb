e0a5c98800a2f00f414bec3f29dea4007d6aa1f0
trinityXmontoya <trinityXmontoya@gmail.com>
2014-04-28 11:10:57 -0400

    remove new method from controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,11 +1,23 @@
 class SessionsController < Application Controller
 
-  def new
-  end
-
   def create
+    @user = User.find_or_create_from_auth_hash(auth_hash)
+    self.current_user = @user
+    @user.id = auth_hash['uid']
+    @user.oauth_token=auth_hash['credentials']['token']
+    @user.name = auth_hash['first_name']
+
+    redirect_to '/'
   end
 
   def destroy
+    session[:user_id] = nil
+    redirect_to '/', notice: "Logged out."
   end
+
+  protected
+  def auth_hash
+    request.env['omniauth.auth']
+  end
+
 end
