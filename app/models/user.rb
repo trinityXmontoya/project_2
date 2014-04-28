090b20d543600abdcd338d36cc307238c9168abf
trinityXmontoya <trinityXmontoya@gmail.com>
2014-04-28 13:54:11 -0400

    add more relationships to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,16 @@
 class User < ActiveRecord::Base
-  has_many :bids, through: :auction
-  has_many :auctions
+  has_many :bids, dependent: :destroy
+  has_many :auctions, dependent: :destroy
+
   has_many :messages, through: :bids
   has_many :messages, through: :auctions
+
+  has_many :categories, through: :auctions
+  has_many :categories, through: :bids
 end
 
+
+# Message.new(user_id: @bid.user_id , messager_id: )
+#
+# @message.bid.user
+# @message.auction.user
