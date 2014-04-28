7d4900a037d95522bf84454d00f673dc2e30b7a0
trinityXmontoya <trinityXmontoya@gmail.com>
2014-04-28 14:19:43 -0400

    message model relationship

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,8 +1,8 @@
 class Message < ActiveRecord::Base
   belongs_to :auction
   belongs_to :bid
-  has_one :user
 
-  validates :content, :user_id, presence: true
+  has_one :user, through: :auction
+  has_one :user, through: :bid
 
 end
