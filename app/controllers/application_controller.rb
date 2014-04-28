6a164ffb98f84a2ff67912373292c9f5aea793f8
trinityXmontoya <trinityXmontoya@gmail.com>
2014-04-28 11:26:08 -0400

    current_user and authenticate helper method creation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,16 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  helper_method :current_user
+  def current_user
+    session[:] ? User.find(session[:user]) : nil
+  end
+
+  def authenticate
+    unless current_user
+    redirect_to root_path, notice: "You must login to continue"
+    end
+  end
+
 end
