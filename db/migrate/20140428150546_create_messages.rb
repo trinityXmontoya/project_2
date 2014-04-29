80ea3149246cec5b8c1f8ea6f6990501ee800660
trinityXmontoya <trinityXmontoya@gmail.com>
2014-04-28 22:59:39 -0400

    change message attribute to content to avoid confusion

--- db/migrate/20140428150546_create_messages.rb
+++ db/migrate/20140428150546_create_messages.rb
@@ -2,8 +2,8 @@ class CreateMessages < ActiveRecord::Migration
   def change
     create_table :messages do |t|
       t.references :user, foreign_key: "sender_id"
-      t.references :user, foreign_key: "receiver_id" 
-      t.text :message
+      t.references :user, foreign_key: "receiver_id"
+      t.text :content
       t.boolean :viewed
       t.timestamps
     end
