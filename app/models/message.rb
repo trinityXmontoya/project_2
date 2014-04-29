2dd8185d4b7590d4d9126f891ef7915549294b11
trinityXmontoya <trinityXmontoya@gmail.com>
2014-04-29 01:10:06 -0400

    add mark_as_viewed method

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,6 +2,7 @@ class Message < ActiveRecord::Base
   belongs_to :sender, class_name: "User", foreign_key: 'sender_id'
   belongs_to :receiver, class_name: "User",foreign_key: 'reciever_id'
 
+
   def mark_as_viewed
     self.update(viewed: true)
   end
