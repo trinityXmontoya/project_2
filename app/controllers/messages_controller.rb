dbfcf3abc16945653128bfb147faf9a1fa607357
trinityXmontoya <trinityXmontoya@gmail.com>
2014-04-29 01:11:07 -0400

    remove unnecessary methods, move update message viewed status to its own method, new create method w ability to handle js

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,15 +1,28 @@
 class MessagesController < ApplicationController
 
   def index
+    @user = User.find(params[:user_id])
+    @messages = @user.messages{auction_id :desc}
   end
 
   def show
-    @message.update(viewed: true)
+    @message = Message.find(params[:message_id])
+    @new_message = Message.new
+    @user = User.find(params[:user_id])
+    unless @message.viewed
+      @message.mark_as_viewed
+    end
   end
 
-  def new
+
+  def create
+    @message = Message.create(content:params[:message][:content],sender_id: params[:message][:auction].user_id,receiver_id: params[:message][:bid].user_id)
+    respond_to do |format|
+      format.html{redirect_to @message}
+      format.js {}
+    end
   end
 
-  def edit
-  end
+
+
 end
