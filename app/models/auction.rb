fcf2cfeb427541716e7c0bd3de31c9bbcb2544a3
trinityXmontoya <trinityXmontoya@gmail.com>
2014-04-29 20:40:37 -0400

    add auction rake task methods to be performed once auction has ended

--- app/models/auction.rb
+++ app/models/auction.rb
@@ -8,28 +8,6 @@ class Auction < ActiveRecord::Base
   belongs_to :user
   belongs_to :auction_participants
 
-
-  def calculate_time_left
-    return Time.now - time_limit
-  end
-end
-
-
-# method:
-
-# is completed
-# check if the auction time limit is 0 then if it is, then set it to completed status to true,
-
-# another method whimenow - created atch calculates what the time limit is, the auction.ti
-
-# if auction.completed is true
-#   time remaining should be in jquery.
-
-#   time.now-time_limit = time_left
-
-
-#   user show controller, make a variable for each thing that the
-
    def time_left
       return time_limit - Time.now
     end
@@ -41,5 +19,42 @@ end
         self.update(completed: true)
         self.save!
       return true
+      end
     end
+
+    def notify_participants
+      #notify the auction creator
+      message_user
+
+      bids.each do |bid|
+        if bid.won
+          #notify winners of the auction
+          message_winner(bid.user)
+        else
+          #notify losers of the auction
+          message_loser(bid.user)
+        end
+    end
+
+    def message_user(bid)
+      Message.auction_user(self,bid.user)
+    end
+
+    def message_winner
+      Message.auction_winner(self,user)
+    end
+
+    def message_loser(bid)
+      Message.auction_loser(self,bid.user)
+    end
+
+    def end_bidding
+      bids.each do |bid|
+        bid.archive_bid
+      end
+    end
+
+    def close_messaging
+    end
+
   end
