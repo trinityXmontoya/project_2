ff3ae5c0e7e3a3afa965e551d26dbd7bc653c956
trinityXmontoya <trinityXmontoya@gmail.com>
2014-04-29 23:31:27 -0400

    fix bid create method

--- app/controllers/bids_controller.rb
+++ app/controllers/bids_controller.rb
@@ -2,13 +2,14 @@ class BidsController < ApplicationController
 
 
   def create
-    @bid=Bid.new(user_id: params[:bid][:user_id], auction_id:params[:bid][:auction_id] , comment: params[:bid][:comment] )
-    if current_user.bids.include? @bid.auction
-      redirect_to @bid.auction, notice: "You may only bid on an auction once."
+    @auction = Auction.find(params[:auction_id])
+    @bid=Bid.new(auction: @auction, user: @auction.user, comment: params[:bid][:comment] )
+    if current_user.bids.include? @auction
+      redirect_to @auction, notice: "You may only bid on an auction once."
     else
       @bid.save
       respond_to do |format|
-        format.html {redirect_to @bid.auction}
+        format.html {redirect_to @auction}
         format.js{}
       end
     end
@@ -20,7 +21,6 @@ class BidsController < ApplicationController
 
   def add_comment
     @bid = Bid.find(params[:id])
-
     @bid.update(comment: params[:bid][:comment])
     respond_to do |format|
       format.html { redirect_to @bid.auction}
