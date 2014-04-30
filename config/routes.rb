398e7e306578a7f6520a6db6508068e918013a3f
trinityXmontoya <trinityXmontoya@gmail.com>
2014-04-30 15:20:49 -0400

    add accept_badge custom route

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,7 @@ Project2::Application.routes.draw do
   resources :categories, only: [:index]
 
   resources :auctions do
+    post '/auctions/:id/accept_badge', to: 'auctions#accept_badge', as: :accept_badge
     resources :bids
     post '/bids/:id/add_comment', to: 'bids#accept_bid',as: :accept_bid
   end
