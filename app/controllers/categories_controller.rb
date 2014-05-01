46c1448b3d44f7193442af1727c4798bfeced980
nplutzik <nplutzik@gmail.com>
2014-04-30 23:50:49 -0400

    Fixed categories model controller

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,15 +1,8 @@
 class CategoriesController < ApplicationController
-<<<<<<< HEAD
+
   def index
     @categories = Category.all
   end
 
   def show
   end
-
-=======
-
-  def index
-    @categories = Category.new
-  end
->>>>>>> 59576171e9e1da99a71997f48239b7e31eadcaa1
