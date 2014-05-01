e9580d58e1f8249c1cd433537c1ff501c73b4507
trinityXmontoya <trinityXmontoya@gmail.com>
2014-05-01 12:13:27 -0400

    auction creation

--- app/controllers/auctions_controller.rb
+++ app/controllers/auctions_controller.rb
@@ -20,7 +20,7 @@ class AuctionsController < ApplicationController
     @auction = Auction.new auction_params
     @auction.add_end_time(@auction.time_end)
     if @auction.save
-      results = @auction.get_location(@auction.location)
+      results = @auction.get_location(@auction.address)
       @auction.save_location(results)
       @auction.save
       redirect_to @auction
@@ -33,7 +33,7 @@ class AuctionsController < ApplicationController
   def edit
     @auction = Auction.find(params[:id])
     if @auction.update
-      latlng = @auction.get_location(@auction.location)
+      latlng = @auction.get_location(@auction.address)
       @auction.save_location(latlng)
       @auction.update
 
@@ -77,8 +77,7 @@ class AuctionsController < ApplicationController
 
   private
   def auction_params
-    params.require(:auction).permit(:user_id, :category_id, :title, :description, :time_end, :time_begin, :event_date, :completed, :viewed, :latitude, :longitude)
-
+    params.require(:auction).permit(:user_id, :category_id, :title, :description, :time_end, :time_begin, :event_date, :completed, :viewed, :latitude, :longitude, :address)
   end
 
 end
