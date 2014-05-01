758323e654c7384b570f00ba659137a1354e3088
trinityXmontoya <trinityXmontoya@gmail.com>
2014-04-30 20:04:13 -0400

    retrieve_user_badges method

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,16 @@
 class Category < ActiveRecord::Base
   belongs_to :auction
 
+
+  def self.retrieve_user_badges(user_badges)
+     badges = []
+     user_badges.each do |badge|
+      badges << Category.find(id: badge)
+    end
+    return badges
+  end
+
+
 end
 
 
