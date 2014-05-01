935bc43923f5d5d3499181b9327a617e28531886
trinityXmontoya <trinityXmontoya@gmail.com>
2014-05-01 15:58:56 -0400

    messaging work

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,11 +1,7 @@
 class MessagesController < ApplicationController
 
-  def index
-    @messages = current_user.messages{auction_id :desc}
-  end
-
   def show
-    @message = Message.find(params[:message_id])
+    @message = Message.find(params[:id])
     @new_message = Message.new
     @user = User.find(params[:user_id])
     unless @message.viewed
@@ -27,13 +23,13 @@ class MessagesController < ApplicationController
     end
   end
 
-  def email_user
-    @message = Message.create(content:params[:message][:content],sender_id: params[:message][:auction].user_id,receiver_id: params[:message][:bid].user_id)
-    respond_to do |format|
-      format.html{redirect_to @message}
-      format.js {}
-    end
-  end
+  # def email_user
+  #   @message = Message.create(content:params[:message][:content],sender_id: params[:message][:auction].user_id,receiver_id: params[:message][:bid].user_id)
+  #   respond_to do |format|
+  #     format.html{redirect_to @message}
+  #     format.js {}
+  #   end
+  # end
 
 
 
