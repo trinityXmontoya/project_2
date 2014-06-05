183e3075bc2b216a4c1f27504be314a48139fd58
alexshook <alexandra.shook@gmail.com>
2014-06-05 16:03:58 -0400

    moving css and js files into vendor, fixed datetimepicker

---
+++ app/assets/javascripts/auctions.js
@@ -0,0 +1,7 @@
+$().ready(function() {
+  $('#datetimepicker1').datetimepicker();
+  $('#datetimepicker2').datetimepicker();
+  $('#datetimepicker3').datetimepicker({
+    pickTime: false
+  });
+});
